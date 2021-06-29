7872a955bf17ae5e8a90ee54295f14851c4118e3
Bhavik Parmar <bhavik2936@gmail.com>
2021-06-29 15:21:34 +0530

    added font-awesome dependency

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 ReactDOM.render(
   <React.StrictMode>
